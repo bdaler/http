f7b98e85c6e458e30960760c6725be60ca796c6e
bdaler <dalerkbtut@gmail.com>
2020-11-09 23:40:43 +0400

    hw1 done

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/bdaler/http/pkg/server"
+	"net"
+	"os"
+)
+
+func main() {
+	if err := execute(server.HOST, server.PORT); err != nil {
+		os.Exit(1)
+	}
+}
+
+func execute(host, port string) (err error) {
+	srv := server.NewServer(net.JoinHostPort(host, port))
+	srv.Register("/", srv.RouteHandler("Welcome to our web-site"))
+	srv.Register("/about", srv.RouteHandler("About Golang Academy"))
+	srv.Register("/payments", srv.RouteHandler("boolshit"))
+	return srv.Start()
+}
